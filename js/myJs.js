4eb10f429686f85d325aa4a8362c0dc564a9eaec
StupidmanTan <40017939+StupidmanTan@users.noreply.github.com>
2023-05-22 15:24:45 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "Chào Lục!",
+  text2: "TAO có điều này muốn hỏi Lục nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Lục yêu Sang123 có phải không nào ._.",
+  text4: "Nếu Lục ko trả lời mà thoát ra tức là muốn làm vợ Sang123 rùi đó nha :v",
+  text5: "Tao Trai Thẳng???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do Lục thích đi :vvvv",
+  text8: "Gửi cho tao <3",
+  text9: "Vì Sang123 đẹp try vlllll",
+  text10: "Ok hỉu rồi",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối nay sang123 qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko cho anh tí RSS",
   text12: "Okii lunn <3",
 };
 
